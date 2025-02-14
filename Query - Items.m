96a431d307753fa22a67e8bacee20700f4c1a82e
Jeff Hall <rhythmatician5@gmail.com>
2025-02-13 19:12:56 -0600

    Refactor queries to access Items table and Lookup view from the new ElectricalUncertaintyDB; Include macros directly instead of referencing ElectricalMacrosWorkbook

--- Query - Items.m
+++ Query - Items.m
@@ -1,7 +1,8 @@
 let
     Source = Odbc.DataSource("dsn=ElectricalUncertaintyDB", [HierarchicalNavigation=true]),
     uncertainty_Database = Source{[Name="uncertainty",Kind="Database"]}[Data],
-    Items_Table = uncertainty_Database{[Name="Items",Kind="Table"]}[Data],
+    public_Schema = uncertainty_Database{[Name="public",Kind="Schema"]}[Data],
+    Items_Table = public_Schema{[Name="Items",Kind="Table"]}[Data],
     #"Removed Other Columns" = Table.SelectColumns(Items_Table,{"Item","Function"})
 in
     #"Removed Other Columns"
--- Query - Lookup.m
+++ Query - Lookup.m
@@ -1,6 +1,7 @@
 let
     Source = Odbc.DataSource("dsn=ElectricalUncertaintyDB", [HierarchicalNavigation=true]),
     uncertainty_Database = Source{[Name="uncertainty",Kind="Database"]}[Data],
-    Lookup_View = uncertainty_Database{[Name="Lookup",Kind="View"]}[Data]
+    public_Schema = uncertainty_Database{[Name="public",Kind="Schema"]}[Data],
+    Lookup_View = public_Schema{[Name="Lookup",Kind="View"]}[Data]
 in
     Lookup_View
